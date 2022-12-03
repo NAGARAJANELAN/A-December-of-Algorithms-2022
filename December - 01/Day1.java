8c62d546b19e522c35baf86cc355c493d491773f
NAGARAJAN E <98300427+NAGARAJANELAN@users.noreply.github.com>
2022-12-03 17:46:06 +0530

    day 1 solution

---
+++ December - 01/Day1.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Day1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        var ob = new Day1();
+        var strArr = new ArrayList<String>();
+        for (int i = 1; i <= n; i++) {
+            strArr.add(sc.next());
+        }
+        ob.getString(strArr);
+    }
+
+    public void getString(ArrayList<String> sarr) {
+
+        for (int i = 0; i < sarr.size(); i++) {
+            String res = "";
+            String[] str = sarr.get(i).split(",");
+            for (int j = 0; j < str.length; j++)
+                res += (char) Integer.parseInt(str[j], 16);
+            System.out.println(res);
+        }
+
+    }
+}
